386d562d131387aa9c9ce7f5429099449c6bb031
edras vallecillo <samir.vallecillo@outlook.com>
2019-09-16 15:55:23 +0000

    Automatically backed up by Learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -32,4 +32,10 @@ end
 
 def valid_phone_number?(phone)
 
+if phone.match(/([0-9] ?){10}/) || phone.match(/(\([0-9]{3}\)([0-9]{3}-[0-9]{4})\b)/) || phone.match(/\b([0-9]{7})\b/)
+    true
+  else
+    false
+  end
+
 end
