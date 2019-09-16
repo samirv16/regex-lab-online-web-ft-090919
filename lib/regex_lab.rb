4f48d1d1cb69cbad856f3c9083944ad26b5d930c
edras vallecillo <samir.vallecillo@outlook.com>
2019-09-16 15:51:46 +0000

    Automatically backed up by Learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -14,26 +14,16 @@ end
 
 def words_five_letters_long(text)
 
- return_array = text.split(" ")
-  return_array.grep(/\b\w{5}\b/)
+ 
 
 end
 
 def first_word_capitalized_and_ends_with_punctuation?(text)
 
-first_letter = text.scan(/\w+/)[0]
-  if (text.scan(/\./)) && (first_letter == first_letter.upcase)
-    true
-  else  
-    false
-  end
+
 
 end
 
 def valid_phone_number?(phone)
-if text.match(/^[A-Z]/) && text.match(/[\.]$/)
-    true
-  else 
-    false
-  end
+
 end
